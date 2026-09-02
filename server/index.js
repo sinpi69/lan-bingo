37b0cbdd553c1f6f61043eff9fcff61a0c4f757a
Prabhav Adhvaryu <adhvaryuprabhav@gmail.com>
2026-09-02 16:33:03 +0530

    index 3001

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import { WebSocketServer } from "ws";
 import { createServer } from "http";
 import crypto from "crypto";
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 const RECONNECT_GRACE_MS = 5 * 60 * 1000;
 const rooms = new Map();
 
